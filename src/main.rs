9113de9b0c9b85622c53a0d6ad11e009a94e8e79
spek <Jk4thefox@gmail.com>
2025-01-15 13:12:51 -0500

    enter number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Welcome to the rust calculator")
+    println!("Welcome to the rust calculator");
+    
+    let mut input = String::new(); //creating a mutable string to hold user input
+
+    
+    io::stdin() //Read user input
+        .read_line(&mut input)
+        .expect("Could read input try agian");
+
+    let input: f64 = input //convert input into a number (f64)
+        .trim() //remove trailign whitespace
+        .parse() //Attempt to convert the string to a floating point number
+        .expect("Invaild number");
+    
+    println!("You entered the number: {}", input);
+
+
 }
